ed6a675abcdf3896f4435d2f77dd77b331d990c3
RenanZortea <renanzorteadefreitas@gmail.com>
2025-12-10 11:48:25 -0300

    feat: Add Expo updates configuration and enable WASM support in Metro.

--- metro.config.js
+++ metro.config.js
@@ -6,4 +6,10 @@ const config = getDefaultConfig(__dirname);
 // Add 'db' to the list of asset extensions
 config.resolver.assetExts.push('db');
 
+// 1. Add 'wasm' to sourceExts so imports like "import ... from './file.wasm'" work
+config.resolver.sourceExts.push('wasm');
+
+// 2. Add 'wasm' to assetExts so the file is included in the bundle
+config.resolver.assetExts.push('wasm');
+
 module.exports = withNativeWind(config, { input: './global.css' });
